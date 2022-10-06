6f7bca6c581bd7eb69f43c0c4c43aa68bfbc75c8
the2en <paff1984@korea.ac.kr>
2022-10-06 21:29:01 +0900

    date

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
+import sys
+from kiwoom.kiwoom import *
+from PyQt5.QtWidgets import *
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+class Main():
+    def __init__(self):
+        print("메인 클래스입니다.")
 
+        self.app = QApplication(sys.argv)    # QApplication 객체 생성
+        self.kiwoom = Kiwoom()
+        self.app.exec_()    # 이벤트 루프 실행.
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    Main()
